3adfbdbb0c37e0fe98ebe6c02e1fc0472d9a9f5a
radiocat <radiocat.app@gmail.com>
2015-12-18 00:43:00 +0900

    refs #6 オプション切り替え * モード切り替えの選択肢をjs側にjsonで定義し動的に設定 * モードの選択値をローカルストレージに保存 * ローカルストレージに保存された値を初期状態で選択にする

--- js/options.js
+++ js/options.js
@@ -1,3 +1,37 @@
 $(function () {
-    $('#dummy').text('jqueryからのダミー');
+    var optionMenu = {
+        "normal": {
+            "title": "標準モード",
+            "value": 0
+        },
+        "shuzo": {
+            "title": "修造モード",
+            "value": 1
+        },
+        "moe": {
+            "title": "萌えモード",
+            "value": 2
+        }
+    };
+
+    $(document).ready(function () {
+        var modeVal = localStorage.getItem('mode');
+        if (modeVal == null) modeVal = 0;
+        $.each(optionMenu, function () {
+            // モードのプルダウンを生成、ローカルストレージに保存された値を選択状態にする
+            $('#opModeMenu').append(
+                $('<li><a href="#" data-value="' + this.value + '">' + this.title + '</a></li>'));
+            if (modeVal == this.value) setSelectedModeTitle(this.title);
+        });
+
+        $('li a', '#opModeMenu').on('click', function () {
+            setSelectedModeTitle($(this).text());
+            localStorage.setItem('mode', $(this).attr("data-value"));
+        });
+    });
+
+    // 選択しているモードの名称をプルダウンの選択値として設定する
+    var setSelectedModeTitle = function (title) {
+        $('#opMode').empty().append(title + ' <span class="caret"></span>');
+    }
 });
