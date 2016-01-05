5649aad334b0222c11dad32e5572251b04258121
radiocat <radiocat.app@gmail.com>
2016-01-06 01:47:50 +0900

    refs #16 配列形式でカスタマイズしたメッセージを登録できる機能を追加

--- js/options.js
+++ js/options.js
@@ -11,8 +11,14 @@ $(function () {
         "moe": {
             "title": "萌えモード",
             "value": 2
+        },
+        "custom": {
+            "title": "カスタム",
+            "value": 3
         }
     };
+
+    $('#opCustom').hide();
 
     $(document).ready(function () {
         var modeVal = localStorage.getItem('mode');
@@ -22,16 +28,52 @@ $(function () {
             $('#opModeMenu').append(
                 $('<li><a href="#" data-value="' + this.value + '">' + this.title + '</a></li>'));
             if (modeVal == this.value) setSelectedModeTitle(this.title);
+            if (modeVal == 3) {
+                showCustomSettings();
+            }
         });
 
         $('li a', '#opModeMenu').on('click', function () {
             setSelectedModeTitle($(this).text());
-            localStorage.setItem('mode', $(this).attr("data-value"));
+            var val = $(this).attr("data-value");
+            if (val == 3) {
+                showCustomSettings();
+            } else {
+                $('#opCustom').hide();
+            }
+            localStorage.setItem('mode', val);
+        });
+        $('.btn', '#opCustom').on('click', function () {
+            var inputVal = $('#opCustomMessage').val();
+            if (!inputVal) {
+                alert('入力してください');
+                return;
+            }
+            try {
+                if (inputVal.length > 5000) alert('こんなところに5000文字以上入力するあなたを励ますのはクリックではなくクリニックをオススメします。');
+                // jsonにパースしてみる。失敗したらエラー。
+                var messageArray = $.parseJSON(inputVal);
+                // jsonの中身が配列だけじゃない場合はエラー
+                if (!Array.isArray(messageArray)) {
+                    console.log("入力値が配列ではないためエラー");
+                    throw new Error();
+                }
+                localStorage.setItem('mode', 3);
+                localStorage.setItem('message', inputVal);
+            } catch (e) {
+                alert('保存できません。入力例を確認してください。');
+            }
+
         });
     });
 
     // 選択しているモードの名称をプルダウンの選択値として設定する
     var setSelectedModeTitle = function (title) {
         $('#opMode').empty().append(title + ' <span class="caret"></span>');
+    }
+
+    var showCustomSettings = function () {
+        $('#opCustomMessage').val(localStorage.getItem('message'));
+        $('#opCustom').show();
     }
 });
--- js/popup.js
+++ js/popup.js
@@ -1,54 +1,70 @@
-(function(){
- $(document).ready(function(){
-     var msgJson = {
-         "normal":{
-             "message":[
-                 "がんばれ",
-                 "負けるな",
-                 "あつくなれよ",
-                 "今が絶好のチャンス！",
-                 "あつくなれよ",
-                 "あつくなれよ"
-             ]
-         },
-         "shuzo":{
-             "message":[
-                 "お前は富士山だ",
-                 "なんでそこであきらめるんだ",
-                 "やればできる"
-             ]
-         },
-         "moe":{
-             "message":[
-                 "おにいちゃんがんばって！",
-                 "べっ、別にアンタのために応援しているわけじゃないからね",
-                 "眼鏡属性って何？"
-             ]
-         }
-     };
-    var modeType = localStorage.getItem('mode');
-    if (modeType == null) modeType = 0;
-    $('#message').text(getYellMessage(parseInt(modeType, 10)));
+(function () {
+    $(document).ready(function () {
+        var msgJson = {
+            "normal": {
+                "message": [
+                    "がんばれ",
+                    "負けるな",
+                    "あつくなれよ",
+                    "今が絶好のチャンス！",
+                    "あつくなれよ",
+                    "あつくなれよ"
+                ]
+            },
+            "shuzo": {
+                "message": [
+                    "お前は富士山だ",
+                    "なんでそこであきらめるんだ",
+                    "やればできる"
+                ]
+            },
+            "moe": {
+                "message": [
+                    "おにいちゃんがんばって！",
+                    "べっ、別にアンタのために応援しているわけじゃないからね",
+                    "眼鏡属性って何？"
+                ]
+            },
+            "custom": {
+                "message": [
+                    ""
+                ]
+            }
+        };
+        var modeType = localStorage.getItem('mode');
+        if (modeType == null) modeType = 0;
+        $('#message').text(getYellMessage(parseInt(modeType, 10)));
 
 
-  function getYellMessage(modeType) {
-    switch (modeType) {
-        case 0:
-            return createMessage("normal");
-        case 1:
-            return createMessage("shuzo");
-        case 2:
-            return createMessage("moe");
-        default:
-              console.log("有効な値ではありません " + modeType);
-              break;
-    }
+        function getYellMessage(modeType) {
+            switch (modeType) {
+                case 0:
+                    return createMessage("normal");
+                case 1:
+                    return createMessage("shuzo");
+                case 2:
+                    return createMessage("moe");
+                case 3:
+                    return createCustomMessage();
+                default:
+                    console.log("有効な値ではありません " + modeType);
+                    break;
+            }
 
-    function createMessage(modeLabel) {
-        var message = msgJson[modeLabel].message;
-        var random = Math.floor( Math.random() * message.length);
-        return message[random];
-    }
-  };
-});
+            function createMessage(modeLabel) {
+                var message = msgJson[modeLabel].message;
+                return getRandomMessage(message);
+            }
+
+            function createCustomMessage() {
+                var message = localStorage.getItem('message');
+                return getRandomMessage($.parseJSON(message));
+            }
+
+            function getRandomMessage(message) {
+                var random = Math.floor(Math.random() * message.length);
+                return message[random];
+            }
+        }
+    });
 })();
